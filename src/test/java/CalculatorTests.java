1750852821d94fcbf332a1b59b8bdb4d8d3af619
20080 <surajkumar2091@gmail.com>
2021-04-27 19:52:16 +0530

    Setup of test file CalculatorTests.java & added Junit4 for testing.
    TDD rule 1: Write no production code, expect to pass a failing test... wrote Calculator.add() even though the code doesn't exists.

---
+++ src/test/java/CalculatorTests.java
@@ -0,0 +1,10 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTests {
+    @Test
+    public void shouldReturnZeroForEmptyString(){
+        Assert.assertEquals(0,Calculator.add());
+    }
+}
